0510365e1469dd3180f70980c4674bf5960d1f33
zmkm374 <n7186703@126.com>
2017-07-15 16:33:39 +0000

    mm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">hello Laravel 5</div>
+                <div class="title">hello Laravel 51</div>
             </div>
         </div>
     </body>
